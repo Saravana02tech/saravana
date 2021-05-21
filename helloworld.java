4ad57a63b31577e7c79a9012eb413b3b223d6d74
Saravana02tech <saravanasurya7931@gmail.com>
2021-05-21 18:27:11 +0530

    saravana

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+class Helloworld {
+public static void main(String[] args) {
+System.out.println("Hello, World!");
+}
+
+
+C:\Users\sarav\.jenkins\workspace\Helloworld
+
+
+C:\Users\sarav\.jenkins\workspace\Helloworld
